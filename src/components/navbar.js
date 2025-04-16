129edc4f61b049cae8d2b0c939695c6df4679b82
Kunal Bhardwaj <kunalrocker62@gmail.com>
2025-04-17 00:36:54 +0530

    navbar updations

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -22,46 +22,50 @@ function NavbarComponent() {
         <BootstrapNavbar bg="dark" variant="dark" expand="lg" className="custom-navbar">
             <Container>
                 <BootstrapNavbar.Brand as={Link} to="/">
-                    Digi Tapri (Your Social media)
+                    Digi Tapri (Your Social Media)
                 </BootstrapNavbar.Brand>
-                <Nav className="ms-auto">
-                    {/* Conditional rendering based on route */}
-                    {!isLoginPage && !isRegisterPage && !isLoggedIn && (
-                        <Button variant="outline-light" as={Link} to="/">
-                            Home
-                        </Button>
-                    )}
-                    {isLoginPage && (
-                        <Button variant="outline-light" as={Link} to="/register">
-                            Register
-                        </Button>
-                    )}
-                    {isRegisterPage && (
-                        <Button variant="outline-light" as={Link} to="/login">
-                            Login
-                        </Button>
-                    )}
-                    
-                    {isHomePage && (
-                        <Button variant="outline-light" as={Link} to="/feed" className="mx-2">
-                            Feed
-                        </Button>
-                    )}
-                    {isFeedPage && (
-                        <Button variant="outline-light"  as={Link} to="/" className="mx-2">
-                            Profile
-                        </Button>
-                    )}
-                    {isLoggedIn && (
-                        <Button variant="outline-light" as={Link} to="/feed" className="mx-2">
-                        Feed
-                    </Button>,
-                        <Button variant="outline-light" onClick={handleLogout} className="mx-2">
-                            Logout
-                        </Button>
-                    
-                    )}
-                </Nav>
+                {/* Hamburger menu toggle button */}
+                <BootstrapNavbar.Toggle aria-controls="navbar-nav" />
+                <BootstrapNavbar.Collapse id="navbar-nav">
+                    <Nav className="ms-auto">
+                        {/* Conditional rendering based on route */}
+                        {!isLoginPage && !isRegisterPage && !isLoggedIn && (
+                            <Button variant="outline-light" as={Link} to="/">
+                                Home
+                            </Button>
+                        )}
+                        {isLoginPage && (
+                            <Button variant="outline-light" as={Link} to="/register">
+                                Register
+                            </Button>
+                        )}
+                        {isRegisterPage && (
+                            <Button variant="outline-light" as={Link} to="/login">
+                                Login
+                            </Button>
+                        )}
+                        {isHomePage && (
+                            <Button variant="outline-light" as={Link} to="/feed" className="mx-2">
+                                Feed
+                            </Button>
+                        )}
+                        {isFeedPage && (
+                            <Button variant="outline-light" as={Link} to="/" className="mx-2">
+                                Profile
+                            </Button>
+                        )}
+                        {isLoggedIn && (
+                            <>
+                                <Button variant="outline-light" as={Link} to="/feed" className="mx-2">
+                                    Feed
+                                </Button>
+                                <Button variant="outline-light" onClick={handleLogout} className="mx-2">
+                                    Logout
+                                </Button>
+                            </>
+                        )}
+                    </Nav>
+                </BootstrapNavbar.Collapse>
             </Container>
         </BootstrapNavbar>
     );
